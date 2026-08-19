7577eee1e07d61c7fff15341185aa7cbcade2a4d
SnYco55 <55nicolasnicolas55@gmail.com>
2026-08-19 11:54:59 +0200

    add: A go back button from AdminView.vue to HomeView.vue

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import router from "@/router";
 </script>
 
 <template>
@@ -7,6 +8,13 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="mx-auto max-w-6xl px-6 py-10">
       <!-- Header -->
       <header class="mb-8">
+        <button
+            type="button"
+            @click="router.back()"
+            class="rounded-xl border border-gray-200 bg-white mb-4 px-4 py-2 text-sm font-medium text-gray-700 shadow-sm transition hover:border-gray-300 hover:bg-gray-50"
+        >
+          Retour
+        </button>
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">Administration</h1>
 
         <p class="mt-2 text-sm text-gray-500">
