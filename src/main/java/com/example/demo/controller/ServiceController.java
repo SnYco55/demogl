13a0db3bc5a7d55e60335769f2150d115b406dff
SnYco55 <55nicolasnicolas55@gmail.com>
2026-08-21 11:00:14 +0200

    add: preparing to deploy, CorsConfig added / logs in API

--- src/main/java/com/example/demo/controller/ServiceController.java
+++ src/main/java/com/example/demo/controller/ServiceController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/services")
 public class ServiceController {
